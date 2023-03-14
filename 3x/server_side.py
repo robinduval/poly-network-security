1920bd8375ba61a92afb33b59a35dc45167aff31
robinduval <leyoda@gmail.com>
2023-03-14 23:29:06 +0100

    Update server side with two errors

--- 3x/server_side.py
+++ 3x/server_side.py
@@ -11,38 +11,41 @@ CLIENT_FILE_RECEIVED_ENCRYPTED = CLIENT_DIR+"encrypted_file.txt"
 CLIENT_FILE_RECEIVED_DECRYPTED = CLIENT_DIR+"decrypted_file.txt"
 
 def receive_file(ip, port):
-	# Crée une socket pour le serveur et écoute les connexions entrantes
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.bind((ip, port))
-	server_socket.listen()
+    # Crée une socket pour le serveur et écoute les connexions entrantes
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((ip, port))
+    server_socket.listen()
 
-	# Accepte une connexion entrante
-	client_socket, addr = server_socket.accept()
+    # Accepte une connexion entrante
+    client_socket, addr = server_socket.accept()
 
-	# Récupère la clé privée du serveur
-	subprocess.run(["openssl", "genpkey", "-algorithm", "RSA", "-out", f"{SERVER_DIR}server_key.pem"])
+    # Récupère la clé privée du serveur
+    subprocess.run(["openssl", "genpkey", "-algorithm", "RSA", "-out", f"{SERVER_DIR}server_key.pem"])
 
-	# Récupère la clé publique envoyée par le client
-	client_key = client_socket.recv(1024)
+    # Récupère la clé publique envoyée par le client
+    client_key = client_socket.recv(1024)
 
-	# Déchiffre la clé symétrique avec la clé privée du serveur
- 	#	subprocess.run(["openssl", "rsautl", "-decrypt", "-inkey", f"{SERVER_DIR}server_key.pem", "-in", f"{CLIENT_DIR}sym_key.enc", "-out", f"{SERVER_DIR}sym_key"])
-	decrypt_key_process = subprocess.run(["openssl", "rsautl", "-decrypt", "-inkey", f"{SERVER_DIR}server_key.pem", "-in", f"{CLIENT_DIR}sym_key.enc", "-out", f"{SERVER_DIR}sym_key"], stderr=subprocess.PIPE)
+    # Déchiffre la clé symétrique avec la clé privée du serveur
+    decrypt_key_process = subprocess.run(["openssl", "rsautl", "-decrypt", "-inkey", f"{SERVER_DIR}server_key.pem", "-in", f"{CLIENT_DIR}sym_key.enc", "-out", f"{SERVER_DIR}sym_key"], stderr=subprocess.PIPE)
 
-	# Check if the decryption process succeeded
-	if decrypt_key_process.returncode != 0:
-		print(f"An error occurred while decrypting the symmetric key: {decrypt_key_process.stderr.decode('utf-8')}")
-		os._exit(1)  # Stop the program
+    # Check if the decryption process succeeded
+    if decrypt_key_process.returncode != 0:
+        print(f"An error occurred while decrypting the symmetric key: {decrypt_key_process.stderr.decode('utf-8')}")
+        os._exit(1)  # Stop the program
 
+    # Déchiffre le fichier avec la clé symétrique
+    decrypt_file_process = subprocess.run(["openssl", "enc", "-d", "-aes-256-cbc", "-in", f"{CLIENT_FILE_RECEIVED_ENCRYPTED}", "-out", f"{CLIENT_FILE_RECEIVED_DECRYPTED}", "-k", open(f"{SERVER_DIR}sym_key", "rb").read()], stderr=subprocess.PIPE)
 
-	# Déchiffre le fichier avec la clé symétrique
-	subprocess.run(["openssl", "enc", "-d", "-aes-256-cbc", "-in", f"{CLIENT_FILE_RECEIVED_ENCRYPTED}", "-out", f"{CLIENT_FILE_RECEIVED_DECRYPTED}", "-k", open(f"{SERVER_DIR}sym_key", "rb").read()])
+    # Check if the decryption process succeeded
+    if decrypt_file_process.returncode != 0:
+        print(f"An error occurred while decrypting the file: {decrypt_file_process.stderr.decode('utf-8')}")
+        os._exit(1)  # Stop the program
 
-	# Ferme la connexion avec le client
-	client_socket.close()
+    # Ferme la connexion avec le client
+    client_socket.close()
 
-	# Ferme la socket du serveur
-	server_socket.close()
+    # Ferme la socket du serveur
+    server_socket.close()
 
 # SERVER : Parrot   : 10.0.0.27
 # CLIENT : RUNBUNTU : 10.0.0.26
