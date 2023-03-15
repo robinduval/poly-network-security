143b48f4cd5c0e522f200e42e5189cb300d1ee00
robinduval <leyoda@gmail.com>
2023-03-15 23:14:52 +0100

    Last Fix

--- 3x/server.py
+++ 3x/server.py
@@ -91,7 +91,7 @@ receive(server_ip, clientsymkey_port, 'client_key_forfile.enc')
 receive(server_ip, clientendata_port, 'raw.enc')
 
 print("Decrypt Client Key")
-decrypt_client_key = subprocess.run(['openssl', 'enc', '-d', '-aes-256-cbc', '-salt', '-in', 'key_forfile.enc', '-out', 'key_forfile.txt.dec', '-pass', 'file:public_key_forkey.pem', '-pbkdf2'], check=True)
+decrypt_client_key = subprocess.run(['openssl', 'enc', '-d', '-aes-256-cbc', '-salt', '-in', 'client_key_forfile.enc', '-out', 'key_forfile.txt.dec', '-pass', 'file:server_public_key_forkey.pem', '-pbkdf2'], check=True)
 if decrypt_client_key.returncode != 0:
     print(f"An error occurred while decrypt_client_key: {decrypt_client_key.stderr.decode('utf-8')}")
     os._exit(1)  # Stop the program
